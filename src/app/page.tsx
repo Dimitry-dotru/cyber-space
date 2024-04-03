b77e62adf756965dad357a465b73031d29265659
Dmitry Shumskiy <ejedima260@gmail.com>
2024-04-03 14:21:46 +0300

    login\logout template added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,18 +20,34 @@ interface userObj {
   timecreated: number;
 }
 
-const getUser = (sessionID: string, setSteamUser: (arg: userObj | null) => void) => {
+const getUser = (sessionID: string, setSteamUser?: (arg: userObj | null) => void) => {
   fetch("http://localhost:7069" + "/?sessionID=" + sessionID).then(d => {
     if (!d.ok) {
+      setSteamUser ? setSteamUser(null) : null;
       throw new Error("User not found!");
     }
     return d.json();
   })
     .then((d: userObj) => {
-      setSteamUser(d);
+      if (setSteamUser) setSteamUser(d);
+      console.log(d);
     }).catch((reason) => {
       console.log(reason);
     })
+}
+
+const logoutHandler = (setSteamUser: (arg: userObj | null) => void) => {
+  const sessionID = window.localStorage.getItem("sessionID");
+  if (!sessionID) return;
+  fetch(`http://localhost:7069/logout/?sessionID=${sessionID}`, { method: "post" }).then(d => {
+    if (!d.ok) {
+      throw new Error("Error with logout!");
+    }
+    window.localStorage.removeItem("sessionID");
+    setSteamUser(null);
+    return d.text();
+  })
+  .then(d => console.log(d));
 }
 
 export default function Home() {
@@ -49,7 +65,7 @@ export default function Home() {
     getUser(sessionID, setSteamUser);
 
     window.localStorage.setItem("sessionID", sessionID);
-    
+
   }, []);
 
   return (
@@ -57,12 +73,15 @@ export default function Home() {
       <div className="testing-block">
         {!steamUser && <a className="btn-secondary" href="http://localhost:7069/api/auth/steam">Login</a>}
 
-        {steamUser && <div className="user-profile">
-          <img src={steamUser.avatarmedium} alt="steam profile photo" />
-          <h3>{steamUser.personaname}</h3>
+        {steamUser && <div className="user-info">
+          <div className="user-profile">
+            <img src={steamUser.avatarmedium} alt="steam profile photo" />
+            <h3>{steamUser.personaname}</h3>
+          </div>
+          <button className="btn-secondary" onClick={() => logoutHandler(setSteamUser)}>Logout</button>
         </div>}
-
       </div>
+
     </main>
   );
 }
