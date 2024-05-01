6023fef8e7896c77c47f086fc50c3196a8286364
Dmitry Shumskiy <ejedima260@gmail.com>
2024-05-02 01:54:12 +0300

    small fixe

--- components/UserBanner/index.tsx
+++ components/UserBanner/index.tsx
@@ -4,49 +4,29 @@ import React from 'react';
 import Image from 'next/image';
 import bunner from '@/public/img/default-imgs/banner_default.webp'
 import avatar from '@/public/img/default-imgs/non_authorised_user.png'
-import edit_banner from '@/public/img/default-imgs/edit_banner.svg'
 import "./style.css";
 
 interface UserBannerProps {
-  // null - значит что пользователь еще не авторизован, значит нужно отобразить дефолтную картинку баннера
-  // и дефолтный аватар
-  // дефолтный баннер: public/img/default-imgs/banner_default.webp
-  // дефолтный аватар: public/img/default-imgs/non_authorised_user.png
   steamUser: userObj | null;
 }
 
 const UserBanner: React.FC<UserBannerProps> = ({
   steamUser
 }) => {
-  // const userBanner = steamUser ? steamUser.userbanner : bunner;
   const handleEditClick = () => {
-    // Обработчик клика по кнопке редактирования
     console.log("Edit button clicked");
   };
-  // тут твой код дальше
   return <>
     <div className="banner-container">
-      <Image src={bunner}  alt="Banner" className="banner" />
+      <Image src={bunner} alt="Banner" className="banner" />
       <Image src={steamUser ? steamUser.avatarfull : avatar} width={250} height={250} alt="Avatar" className="avatar" />
-      <button className="edit-button"  onClick={handleEditClick}>
+      <button className="edit-button" onClick={handleEditClick}>
         <div className="edit-box-circle">
-      <span className="material-symbols-outlined">edit</span>
-      </div>
+          <span className="material-symbols-outlined">edit</span>
+        </div>
       </button>
     </div>
   </>
-
-
-  // в будущем, в steamUser будет поле userbanner, в котором и будет путь к картинке нужной
-  // а пока, сделай переменную userBanner, которой ты присвоишь дефолтную картинку баннера, чтобы я потом простенько
-  // это все заменил
-}
-
-// это будет вот этот синий карандашик: https://prnt.sc/DbQ7z4U7GGBM
-const EditBanner: React.FC = () => {
-  return <>
-
-  </>;
 }
 
 export default UserBanner;
