5c8ac772c55e42b8c1a2f5d7c2bff8e8ed48d8e7
Dmitry Shumskiy <ejedima260@gmail.com>
2024-05-20 14:40:34 +0300

    text editor: begining

--- components/ShareYourThoughts/index.tsx
+++ components/ShareYourThoughts/index.tsx
@@ -1,4 +1,7 @@
 import { userObj } from "@/src/utils/types/steamTypes";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import "./style.css";
 
 import userLogo from "@/public/img/default-imgs/non_authorised_user.png";
@@ -8,14 +11,43 @@ interface ShareYourThoughtsProps {
 }
 
 const ShareYourThoughts: React.FC<ShareYourThoughtsProps> = ({ steamUser }) => {
+  const [value, setValue] = useState("");
 
-  return <div className="posts-container">
-    <div className="input-logo-container">
-      <img src={steamUser ? steamUser.avatarmedium : userLogo.src} className="profile-picture" />
-      <input disabled={!steamUser} type="text" id="searchInput" placeholder={steamUser ? "Share Your Thoughts...": "You can't share your thoughts yet!"} />
-    </div>
-    <span className="material-symbols-outlined">edit</span>
-  </div>;
+
+  const modules = {
+    toolbar: {
+      container: [
+        [{ "header": "1" }, { "header": "2" }],
+        [{ size: ["small", "normal", "large"] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ "list": "ordered" }, { "list": "bullet" },
+        { "indent": "-1" }, { "indent": "+1" }],
+        ["link", "image"],
+        ["clean"]
+      ],
+    }
+  }
+
+  return (
+    <ReactQuill
+      value={value}
+      onChange={setValue}
+      modules={modules}
+      formats={[
+        "header", "font", "size",
+        "bold", "italic", "underline", "strike", "blockquote",
+        "list", "bullet", "indent",
+        "link", "image"
+      ]}
+    />
+  );
+  // return <div className="posts-container">
+  //   <div className="input-logo-container">
+  //     <img src={steamUser ? steamUser.avatarmedium : userLogo.src} className="profile-picture" />
+  //     <input disabled={!steamUser} type="text" id="searchInput" placeholder={steamUser ? "Share Your Thoughts...": "You can"t share your thoughts yet!"} />
+  //   </div>
+  //   <span className="material-symbols-outlined">edit</span>
+  // </div>;
 }
 
 export default ShareYourThoughts;
