dded5998877affcc03672bba087336ec5e0a674c
Dmitry Shumskiy <ejedima260@gmail.com>
2024-05-22 10:27:38 +0300

    text editor styles complete

--- components/ShareYourThoughts/index.tsx
+++ components/ShareYourThoughts/index.tsx
@@ -1,5 +1,5 @@
 import { userObj } from "@/src/utils/types/steamTypes";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./style.css";
@@ -12,6 +12,16 @@ interface ShareYourThoughtsProps {
 
 const ShareYourThoughts: React.FC<ShareYourThoughtsProps> = ({ steamUser }) => {
   const [value, setValue] = useState("");
+  const [isEditorOpen, setIsEditorOpen] = useState<boolean>(false);
+
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    if (quillRef.current && isEditorOpen) {
+      const editor = (quillRef.current as any).getEditor();
+      editor.focus();
+    }
+  }, [isEditorOpen]);
 
 
   const modules = {
@@ -29,26 +39,36 @@ const ShareYourThoughts: React.FC<ShareYourThoughtsProps> = ({ steamUser }) => {
     }
   }
 
+  if (!isEditorOpen) return <div className="posts-container">
+    <div className="input-logo-container">
+      <img src={steamUser ? steamUser.avatarmedium : userLogo.src} className="profile-picture" />
+      <input onFocus={(e) => setIsEditorOpen(true)} disabled={!steamUser} type="text" id="searchInput" placeholder={steamUser ? "Share Your Thoughts..." : "You can't share your thoughts yet!"} />
+    </div>
+    <span className="material-symbols-outlined">edit</span>
+  </div>
+
+
   return (
-    <ReactQuill
-      value={value}
-      onChange={setValue}
-      modules={modules}
-      formats={[
-        "header", "font", "size",
-        "bold", "italic", "underline", "strike", "blockquote",
-        "list", "bullet", "indent",
-        "link", "image", "align"
-      ]}
-    />
+    <div className="text-editor-container">
+      <ReactQuill
+        ref={quillRef}
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        placeholder="Start typing here..."
+        formats={[
+          "header", "font", "size",
+          "bold", "italic", "underline", "strike", "blockquote",
+          "list", "bullet", "indent",
+          "link", "image", "align"
+        ]}
+      />
+      <button className="close-btn" onClick={() => {
+        setIsEditorOpen(false);
+        setValue("");
+      }}></button>
+    </div>
   );
-  // return <div className="posts-container">
-  //   <div className="input-logo-container">
-  //     <img src={steamUser ? steamUser.avatarmedium : userLogo.src} className="profile-picture" />
-  //     <input disabled={!steamUser} type="text" id="searchInput" placeholder={steamUser ? "Share Your Thoughts...": "You can"t share your thoughts yet!"} />
-  //   </div>
-  //   <span className="material-symbols-outlined">edit</span>
-  // </div>;
 }
 
 export default ShareYourThoughts;
